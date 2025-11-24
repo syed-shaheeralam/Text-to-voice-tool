ad1fbecd3844c89a0715f2f8178b824522e07fec
Wajeeha Nadeem <Jiaaya@users.noreply.huggingface.co>
2025-11-24 12:54:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ import uuid
 
 app = FastAPI()
 
-# Multi-voice model (VCTK, requires espeak backend)
+# Multi-voice model (requires espeak-ng for Windows)
 tts = TTS(model_name="tts_models/en/vctk/vits", progress_bar=False)
 
 @app.post("/generate")
 async def generate_voice(text: str = Form(...), speaker: str = Form("p226")):
     """
     Generate TTS audio
-    - 'text': text to convert
-    - 'speaker': choose from tts.speakers (boys, girls, kids, old, cinematic)
+    - text: text to convert
+    - speaker: choose from tts.speakers (kids, boy, girl, old, cinematic)
     """
     file_name = f"{uuid.uuid4()}.wav"
     tts.tts_to_file(text=text, speaker=speaker, file_path=file_name)
